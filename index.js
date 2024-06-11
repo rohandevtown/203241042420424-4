b6032510ba8c10ee1a48537a53e1e873abb344e9
rohandevtown <rohankinnal@devtown.in>
2024-06-11 19:09:38 +0530

    mongo

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const {users} = require("./data/users.json");
 
+const dotenv = require("dotenv")
+
+// Database Connection
+const DbConnection = require("./databaseConnection")
+
 // importing routes
 const usersRouter = require("./routes/users");
 const booksRouter = require("./routes/books");
 
+dotenv.config();
+
 const app = express();
+
+DbConnection();
 
 const PORT = 8081;
 
